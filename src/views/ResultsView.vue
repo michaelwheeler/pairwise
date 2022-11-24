3ca3c027865a3bdf0b708ef886c748d3156b3cc6
Michael Wheeler <mpwheel@umich.edu>
2022-11-24 11:13:45 -0500

    Show results on results page

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useComparisonStore } from "@/stores/comparison";
+
+const store = useComparisonStore();
+const { results } = storeToRefs(store);
+</script>
+
 <template>
   <div>
     <h1>Results</h1>
+    <ol v-if="results.length">
+      <li v-for="candidate in results" :key="candidate">{{ candidate }}</li>
+    </ol>
+    <div v-else>
+      <p>No results yet.</p>
+    </div>
   </div>
 </template>
